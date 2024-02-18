134ca3a7546b007ccaa5470bdeca69eed5d1e1e8
Sanuth23 <sanuth0000@gmail.com>
2024-02-18 10:08:28 +0530

    setup login page titlt html

---
+++ fb-login/src/Login.tsx
@@ -0,0 +1,19 @@
+import './Login.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function Login() {
+    return (
+        <>
+            <div className="content">
+                <div className="flex-div">
+                    <div className="name-content">
+                        <h1 className="logo">Facebook</h1>
+                        <p>Connect with friends and the world around you on Facebook.</p>
+                    </div>
+                   
+                </div>
+            </div>
+        </>
+    );
+};
+export default Login
