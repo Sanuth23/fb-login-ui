5113f767c11b555d6ee0d794b81c642bfb487884
Sanuth23 <sanuth0000@gmail.com>
2024-02-18 11:23:17 +0530

    adding bootstrap

--- fb-login/src/Login.tsx
+++ fb-login/src/Login.tsx
@@ -4,22 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function Login() {
     return (
         <>
-            <div className="content">
-                <div className="flex-div">
-                    <div className="name-content">
-                        <h1 className="logo">Facebook</h1>
-                        <p>Connect with friends and the world around you on Facebook.</p>
+            <body>
+                <div className="container m-5">
+                    <div className="justify-content-center row">
+                        <div className="col-lg-6 col-sm-12">
+                            <h1 className="logo">facebook</h1>
+                            <p>Connect with friends and the world around you on Facebook.</p>
+                        </div>
+                        <div className="col-lg-6 col-sm-12">
+                            <form>
+                                <input type="text" placeholder="Email or Phone Number" required />
+                                <input type="password" placeholder="Password" required />
+                                <button className="login">Log In</button>
+                                <a href="#">Forgot Password ?</a>
+                                <hr />
+                                <button className="create-account">Create New Account</button>
+                            </form>
+                        </div>
                     </div>
-                    <form>
-                        <input type="text" placeholder="Email or Phone Number" required />
-                        <input type="password" placeholder="Password" required />
-                        <button className="login">Log In</button>
-                        <a href="#">Forgot Password ?</a>
-                        <hr />
-                        <button className="create-account">Create New Account</button>
-                    </form>
                 </div>
-            </div>
+            </body>
         </>
     );
 };
