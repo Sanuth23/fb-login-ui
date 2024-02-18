24d2fcbb361db289aeb80a699383b9d7c03b33c8
Sanuth23 <sanuth0000@gmail.com>
2024-02-18 11:29:07 +0530

    bug fix in css

--- fb-login/src/Login.tsx
+++ fb-login/src/Login.tsx
@@ -4,26 +4,24 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function Login() {
     return (
         <>
-            <body>
-                <div className="container m-5">
-                    <div className="justify-content-center row">
-                        <div className="col-lg-6 col-sm-12">
-                            <h1 className="logo">facebook</h1>
-                            <p>Connect with friends and the world around you on Facebook.</p>
-                        </div>
-                        <div className="col-lg-6 col-sm-12">
-                            <form>
-                                <input type="text" placeholder="Email or Phone Number" required />
-                                <input type="password" placeholder="Password" required />
-                                <button className="login">Log In</button>
-                                <a href="#">Forgot Password ?</a>
-                                <hr />
-                                <button className="create-account">Create New Account</button>
-                            </form>
-                        </div>
+            <div className="container m-5">
+                <div className="justify-content-center row">
+                    <div className="col-lg-6 col-sm-12">
+                        <h1 className="logo">facebook</h1>
+                        <p>Connect with friends and the world around you on Facebook.</p>
+                    </div>
+                    <div className="col-lg-6 col-sm-12">
+                        <form>
+                            <input type="text" placeholder="Email or Phone Number" required />
+                            <input type="password" placeholder="Password" required />
+                            <button className="login">Log In</button>
+                            <a href="#">Forgot Password ?</a>
+                            <hr />
+                            <button className="create-account">Create New Account</button>
+                        </form>
                     </div>
                 </div>
-            </body>
+            </div>
         </>
     );
 };
